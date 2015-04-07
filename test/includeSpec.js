4417bed4ce5f89fc619f83139928596fe55dad05
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-07 11:08:07 +0100

    Created tests to verify correct data is returned for the request and an error is made for an incorrect request

--- test/includeSpec.js
+++ test/includeSpec.js
@@ -1,19 +1,45 @@
-var expect = require('chai').expect;
+var expect  = require('chai').expect;
+var nock    = require('nock');
 var include = require('../lib/include.js');
+var appendToURL = include.appendToURL;
+var makeRequest = include.makeRequest;
 
 describe('include', function() { 
 
-  var root = 'https://blockchain.info/';
+  var url = 'https://blockchain.info/';
+  var json = {
+              "hash":"00000000000000",
+              "time":1428398380,
+              "block_index":829202,
+              "height":351097,
+              "txIndexes":[82937058,82937904]
+              }
+  var correcRequest = nock(url)
+                        .get('/')
+                        .reply(200, json);
 
   it('should return an empty string if there is no value to append', function() { 
-    var url = include.appendToURL('api_code');
+    var url = appendToURL('api_code');
     expect(url).to.equal('');
   });
 
-  it('should append the correct query string to the url', function() { 
-    var query = include.appendToURL('api_code', '12345');
+  it('should create the correct query string', function() { 
+    var query = appendToURL('api_code', '12345');
     expect(query).to.equal('&api_code=12345');
   });
 
+  it('should return the correct data for the request', function(done) { 
+    makeRequest(url, function(err, data) { 
+      expect(data).to.eql(json);
+      done();
+    });
+  });
+
+  it('should return an error if the request cannot be handled', function(done) { 
+    makeRequest('http://idontexist.com', function(err, data) { 
+      expect(err).to.be.ok;
+      done();
+    });
+  });
 
 });
