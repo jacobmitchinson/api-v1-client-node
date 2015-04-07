a5a36bb237b7a08a1f9ccc32225ea70c168d3d0b
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-04-07 10:11:05 +0100

    Added test to check query string is being made correctly

--- test/includeSpec.js
+++ test/includeSpec.js
@@ -3,9 +3,16 @@ var include = require('../lib/include.js');
 
 describe('include', function() { 
 
+  var root = 'https://blockchain.info/';
+
   it('should return an empty string if there is no value to append', function() { 
     var url = include.appendToURL('api_code');
     expect(url).to.equal('');
+  });
+
+  it('should append the correct query string to the url', function() { 
+    var query = include.appendToURL('api_code', '12345');
+    expect(query).to.equal('&api_code=12345');
   });
 
 
